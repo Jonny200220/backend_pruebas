ca9e2ac9c45e328c09d9f6702984bb753ba915eb
Jonny <jonnyrmzhdz@gmail.com>
2024-10-28 14:20:44 -0600

    agregando crud de tipos con sus rutas y modificacion a la ruta de marcas a resource

---
+++ app/Http/Controllers/TiposController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tipos;
+use Illuminate\Http\Request;
+
+class TiposController extends Controller
+{
+    public function index()
+    {
+        $consulta = Tipos::all();
+        return response()->json(['data' => $consulta,'message' => 'success'], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $marca = new Tipos();
+        $marca->descripcion_tipo = $request->input('descripcion_tipo');
+        $marca->REGISTRO_fecha_creacion = now();
+        $marca->REGISTRO_fecha_ultimo_cambio = now();
+        $marca->REGISTRO_en_uso = $request->input('REGISTRO_en_uso');
+        $marca->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Tipo creado exitosamente'
+        ], 201); // 201 para creación exitosa
+    }
+
+    public function update(Request $request, Tipos $tipo){
+
+        $tipo->update([
+            'descripcion_tipo' => $request->input('descripcion_tipo'),
+            'REGISTRO_fecha_ultimo_cambio' => now(),
+            'REGISTRO_en_uso' => $request->input('REGISTRO_en_uso')
+        ]);
+        return response()->json([
+            'status' => true,
+            'message' => 'Tipo actualizado exitosamente'
+        ], 200);
+    }
+
+    public function destroy(Tipos $tipo){
+        $tipo->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Tipo eliminado exitosamente'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DescripcionesController;
 use App\Http\Controllers\MarcasController;
 use App\Http\Controllers\ModelosController;
 use App\Http\Controllers\PruebaController;
+use App\Http\Controllers\TiposController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,13 +23,7 @@ Route::post('v1/insert', [PruebaController::class, 'readAndInsert']);
 
 Route::get('v1/progreso', [PruebaController::class, 'getProgreso']);
 
-// Route::resource('v1/marcas', MarcasController::class);
-
-Route::get('v1/marcas', [MarcasController::class, 'index']);
-Route::post('v1/marcas', [MarcasController::class, 'store']);
-// Route::get('v1/marcas/{id}', [MarcasController::class, 'show']); 
-Route::put('v1/marcas/{marca}', [MarcasController::class, 'update']); 
-Route::delete('v1/marcas/{marca}', [MarcasController::class, 'destroy']);
+Route::resource('v1/marcas', MarcasController::class);
 
 Route::get('v1/modelos', [ModelosController::class, 'index']);
 Route::post('v1/modelos', [ModelosController::class, 'store']);
@@ -41,3 +36,9 @@ Route::post('v1/descripciones', [DescripcionesController::class, 'store']);
 // Route::get('v1/marcas/{id}', [MarcasController::class, 'show']); 
 Route::put('v1/descripciones/{descripcion}', [DescripcionesController::class, 'update']); 
 Route::delete('v1/descripciones/{descripcion}', [DescripcionesController::class, 'destroy']);
+
+Route::get('v1/tipos', [TiposController::class, 'index']);
+Route::post('v1/tipos', [TiposController::class, 'store']);
+// Route::get('v1/marcas/{id}', [MarcasController::class, 'show']); 
+Route::put('v1/tipos/{tipo}', [TiposController::class, 'update']); 
+Route::delete('v1/tipos/{tipo}', [TiposController::class, 'destroy']);
