e9e1c3540b15297ab2b57b42b1d8095301373df6
Jonny <jonnyrmzhdz@gmail.com>
2024-10-29 09:09:01 -0600

    cambiando rutas a resource

--- routes/api.php
+++ routes/api.php
@@ -25,20 +25,8 @@ Route::get('v1/progreso', [PruebaController::class, 'getProgreso']);
 
 Route::resource('v1/marcas', MarcasController::class);
 
-Route::get('v1/modelos', [ModelosController::class, 'index']);
-Route::post('v1/modelos', [ModelosController::class, 'store']);
-// Route::get('v1/marcas/{id}', [MarcasController::class, 'show']); 
-Route::put('v1/modelos/{modelo}', [ModelosController::class, 'update']); 
-Route::delete('v1/modelos/{modelo}', [ModelosController::class, 'destroy']);
+Route::resource('v1/modelos', ModelosController::class);
 
-Route::get('v1/descripciones', [DescripcionesController::class, 'index']);
-Route::post('v1/descripciones', [DescripcionesController::class, 'store']);
-// Route::get('v1/marcas/{id}', [MarcasController::class, 'show']); 
-Route::put('v1/descripciones/{descripcion}', [DescripcionesController::class, 'update']); 
-Route::delete('v1/descripciones/{descripcion}', [DescripcionesController::class, 'destroy']);
+Route::resource('v1/descripciones', DescripcionesController::class);
 
-Route::get('v1/tipos', [TiposController::class, 'index']);
-Route::post('v1/tipos', [TiposController::class, 'store']);
-// Route::get('v1/marcas/{id}', [MarcasController::class, 'show']); 
-Route::put('v1/tipos/{tipo}', [TiposController::class, 'update']); 
-Route::delete('v1/tipos/{tipo}', [TiposController::class, 'destroy']);
+Route::resource('v1.tipos', TiposController::class);
